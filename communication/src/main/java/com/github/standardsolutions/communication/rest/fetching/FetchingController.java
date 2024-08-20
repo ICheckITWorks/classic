e3d3a3a166fdce69c0d4fc881db76a1bf1bb95cd
Merkurev Sergei <MerkurevSergei@yandex.ru>
2024-08-21 06:12:46 +0900

    [STSL-7] Добавлен get метод проверки фильтрации

--- communication/src/main/java/com/github/standardsolutions/communication/rest/fetching/FetchingController.java
+++ communication/src/main/java/com/github/standardsolutions/communication/rest/fetching/FetchingController.java
@@ -2,9 +2,11 @@ package com.github.standardsolutions.communication.rest.fetching;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.standardsolutions.request.FetchRequest;
 
@@ -17,6 +19,15 @@ public class FetchingController {
 
     @PostMapping
     public Page<FlightsEntity> getVacancies(@RequestBody FetchRequest request) {
+        return flightsRepository.findAll(request.toSpecification(), request.toPageable());
+    }
+
+    @GetMapping
+    public Page<FlightsEntity> getVacancies2(@RequestParam(required = false) String filters,
+                                             @RequestParam(required = false) String sort,
+                                             @RequestParam(required = false) Integer pageNumber,
+                                             @RequestParam(required = false) Integer pageSize) {
+        FetchRequest request = new FetchRequest(filters, sort, pageNumber, pageSize);
         return flightsRepository.findAll(request.toSpecification(), request.toPageable());
     }
 }
